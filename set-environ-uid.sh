7de8a714b07ad3ef732d5f8aacc134d9a307c90f
niu atsushi <atsushi.new414@gmail.com>
2021-11-03 00:37:40 +0900

    コンテナ起動時に必要ライブラリをインストールするよう修正

--- set-environ-uid.sh
+++ set-environ-uid.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# ホスト側ユーザーのUIDとGIDを.envファイルに事前に書き出しておく
+# ここで書き出したUIDとGIDでmysqlコンテナ内でフォルダ捜査権限を与える
 echo "LINUX_MYSQL_UID=$(id -u $USER)" >> .env
 echo "LINUX_MYSQL_GID=$(id -g $USER)" >> .env
