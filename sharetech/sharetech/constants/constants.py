fbf240112c3c9f17cf2865518b25bd14c373a9c7
niu atsushi <atsushi.new414@gmail.com>
2021-11-24 07:15:58 +0900

    トップページデザイン修正中

--- sharetech/sharetech/constants/constants.py
+++ sharetech/sharetech/constants/constants.py
@@ -2,4 +2,14 @@ class Constants:
     # アプリケーション内での固定値を保持する
 
     # ローカル環境での記事画像データ保存先
-    IMG_ACCESS_URL = '../sharetech/'
+    PROJECT_URL = '/sharetech/'
+    IMG_DIR = 'img/'
+
+    @classmethod
+    def get_static_file_path(self):
+        return '..' + self.PROJECT_URL
+
+    @classmethod
+    def get_image_path(self):
+        image_path = self.PROJECT_URL + self.IMG_DIR
+        return image_path
--- sharetech/sharetech/utils/model_template_adapter.py
+++ sharetech/sharetech/utils/model_template_adapter.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from sharetech.models import *
+from sharetech.constants import Constants
 
 class BaseModelTemplateAdapter(ABC):
     '''
@@ -14,7 +15,7 @@ class ConsultWindodwAdapter(BaseModelTemplateAdapter):
     '''
     DB抽出データをtemplateで展開できる形に変換する
     '''
-    DEFAULT_USER_ICON_PATH = 'default_icon.png'
+    DEFAULT_USER_ICON_PATH = 'default_avater.png'
 
     def __init__(self, consult_window_models: ConsultWindow):
         self.__consult_window_models = consult_window_models
@@ -25,7 +26,7 @@ class ConsultWindodwAdapter(BaseModelTemplateAdapter):
         for index, consult_window_model in enumerate(self.__consult_window_models):
             consult_window_content = {
                 'number' : str(index + 1),
-                'expert_icon_path' : cconsult_window_model.expert_user_id.icon_path if consult_window_model.expert_user_id.icon_path != None else self.DEFAULT_USER_ICON_PATH,
+                'expert_icon_path' : Constants.get_image_path() + consult_window_model.expert_user_id.icon_path if consult_window_model.expert_user_id.icon_path != None else Constants.get_image_path() + self.DEFAULT_USER_ICON_PATH,
                 'created_at' : consult_window_model.created_at,
                 'title' : consult_window_model.consult_window_title,
                 'applyed_num' : consult_window_model.applyed_num,
