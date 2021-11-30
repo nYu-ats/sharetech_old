a1b76870dc0a9881d395035dfd5e2611e70b98fe
MasayukiNae <masayuki.ishikawa@ulsystems.co.jp>
2021-11-30 23:28:13 +0900

    htmlレベルで相談窓口登録画面作成2

--- sharetech/sharetech/forms/adviser_register_form.py
+++ sharetech/sharetech/forms/adviser_register_form.py
@@ -17,40 +17,13 @@ class AdviserRegisterForm(ModelForm):
             'consult_window_title', 
             'consult_window_overview', 
             'consult_price', 
-            'timerex_url', 
-            'archivement', 
+            'archivement',
+            # 'expert_user_id', 
             ]
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', '')
         super().__init__(*args, **kwargs)
-
-    def clean(self):
-        name = self.cleaned_data.get('name')
-        organization = self.cleaned_data.get('organization')
-        special = self.cleaned_data.get('special')
-        email = self.cleaned_data.get('email')
-
-        # if email is not None and password:
-        #     self.user_cache = authenticate(self.request, email=email, password=password)
-        #     if self.user_cache is None:
-        #         raise self.get_invalid_login_error()
-        #     else:
-        #         self.confirm_login_allowed(self.user_cache)
-    
-    # def confirm_login_allowed(self, user):
-    #     if user.is_deleted:
-    #         raise forms.ValidationError(
-    #             self.error_messages['is_deleted'],
-    #             code = 'deleted',
-    #         )
     
     def get_user(self):
         return self.user_cache
-    
-    # def get_invalid_login_error(self):
-    #     return forms.ValidationError(
-    #         self.error_messages['invalid_login'],
-    #         code = 'invalid_login',
-    #         params = {'username' : gettext_lazy('Email')},
-    #     )
--- sharetech/sharetech/views/AdviserRegisterView.py
+++ sharetech/sharetech/views/AdviserRegisterView.py
@@ -1,17 +1,33 @@
 from django.views import generic
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from ..forms.adviser_register_form import AdviserRegisterForm
+from sharetech.utils.model_template_adapter import ConsultWindodwAdapter
+from sharetech.models import *
 
 # ユーザーモデル(カスタム)取得
 User = get_user_model()
 
-class AdviserRegisterView(generic.CreateView):
+class AdviserRegisterView(LoginRequiredMixin, View):
 
     # 専門家登録窓口フォーム
-    form_class = AdviserRegisterForm
     template_name = 'sharetech/adviser_register.html'
+    form_class = AdviserRegisterForm
+    model = ConsultWindow
+
+    def get(self, request, *args, **kwargs):
+        params = {'form': AdviserRegisterForm()}
+        return render(request, 'sharetech/adviser_register.html', params)
+
+    def post(self, request, *args, **kwargs):
+        if request.method == "POST":
+            print(self.request.user)
+        return render(request, 'sharetech/adviser_register.html')
 
     def form_valid(self, form):
-        user = form.save(commit=False)
+        print(self.request.user)
+        return super().form_valid(form)
 
 adiviser_register = AdviserRegisterView.as_view()
