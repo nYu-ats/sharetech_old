3e88461b769bf67881447bb66731976e2fbac0e3
MasayukiNae <masayuki.ishikawa@ulsystems.co.jp>
2021-12-04 13:10:33 +0900

    相談窓口登録機能完成

--- sharetech/sharetech/forms/adviser_register_form.py
+++ sharetech/sharetech/forms/adviser_register_form.py
@@ -1,10 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from django.forms.fields import EmailField
 from sharetech.models.consult_window import ConsultWindow
-from django.contrib.auth import (get_user_model, authenticate)
-from django.utils.translation import gettext_lazy
-from django.utils.text import capfirst
 
 class AdviserRegisterForm(ModelForm):
     '''
@@ -18,8 +14,10 @@ class AdviserRegisterForm(ModelForm):
             'consult_window_overview', 
             'consult_price', 
             'archivement',
-            # 'expert_user_id', 
             ]
+        widgets = {
+            'consult_window_overview': forms.Textarea(),
+        }
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', '')
--- sharetech/sharetech/views/adviser_register_view.py
+++ sharetech/sharetech/views/adviser_register_view.py
@@ -1,10 +1,9 @@
-from django.views import generic
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
 from ..forms.adviser_register_form import AdviserRegisterForm
-from sharetech.utils.model_template_adapter import ConsultWindodwAdapter
+from sharetech.utils.model_template_adapter import *
 from sharetech.models import *
 
 # ユーザーモデル(カスタム)取得
@@ -14,20 +13,35 @@ class AdviserRegisterView(LoginRequiredMixin, View):
 
     # 専門家登録窓口フォーム
     template_name = 'sharetech/adviser_register.html'
-    form_class = AdviserRegisterForm
-    model = ConsultWindow
+    params = {}
 
     def get(self, request, *args, **kwargs):
-        params = {'form': AdviserRegisterForm()}
-        return render(request, 'sharetech/adviser_register.html', params)
+        self.params.clear()
+        category_list = list(CategoryMst.objects.order_by('id'))
+        self.params.update(
+                {
+                'form': AdviserRegisterForm(),
+                'category_dict' : CategoryAdapter.convert_to_template_context(category_list),
+                }
+            )
+        return render(request, 'sharetech/adviser_register.html', self.params)
 
     def post(self, request, *args, **kwargs):
-        if request.method == "POST":
-            print(self.request.user)
-        return render(request, 'sharetech/adviser_register.html')
-
-    def form_valid(self, form):
-        print(self.request.user)
-        return super().form_valid(form)
+        # 相談窓口を登録
+        consultWindow = ConsultWindow(
+            expert_user_id = CustomUser.objects.get(id=self.request.user.id),
+            consult_window_title = self.request.POST.get("consult_window_title"),
+            consult_window_overview = self.request.POST.get("consult_window_overview"),
+            consult_price = self.request.POST.get("consult_price"),
+            archivement = self.request.POST.get("archivement")
+        )
+        consultWindow.save()
+        # カテゴリと相談窓口のマッピングを登録
+        for cat_id in self.request.POST.getlist("cat"):
+            CategoryConsultWindowMapping.objects.create(
+                category_id = CategoryMst.objects.get(id=cat_id),
+                consult_window_id = consultWindow
+            )
+        return render(request, 'sharetech/adviser_register.html', self.params)
 
 adiviser_register = AdviserRegisterView.as_view()
