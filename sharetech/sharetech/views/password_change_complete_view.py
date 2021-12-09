58f5bfd6efdd5e933f22e36c8c52398f421a1ded
niu atsushi <atsushi.new414@gmail.com>
2021-12-10 00:07:13 +0900

    ログインログアウト状態制御修正

--- sharetech/sharetech/views/password_change_complete_view.py
+++ sharetech/sharetech/views/password_change_complete_view.py
@@ -7,6 +7,8 @@ class PasswordChangeCompleteView(generic.TemplateView):
         context = super().get_context_data()
         # TODO メッセージは1ファイルで管理するようにする
         context['message'] = 'パスワード変更が完了しました'
+        # ログアウト状態を示すフラグ
+        context['logout'] = True
         return context
 
 password_change_complete = PasswordChangeCompleteView.as_view()
