b4a4f84bf870fadc313bcdf824de0f1604dc9433
niu atsushi <atsushi.new414@gmail.com>
2021-12-16 04:02:38 +0900

    userモデルカラム修正影響対応

--- sharetech/sharetech/views/consult_window_detail_view.py
+++ sharetech/sharetech/views/consult_window_detail_view.py
@@ -50,7 +50,7 @@ class ConsultWindowDetailView(BasePageCommonView):
                 'expert_user_id' : consult_window_detail.expert_user_id.id,
                 'icon_path' : Constants.get_image_path() + consult_window_detail.expert_user_id.icon_path if consult_window_detail.expert_user_id.icon_path != None else ImageConstants.get_default_icon_path(),
                 'company' : consult_window_detail.expert_user_id.company,
-                'username' : consult_window_detail.expert_user_id.username_kana,
+                'username' : consult_window_detail.expert_user_id.username,
                 'occupation' : consult_window_detail.expert_user_id.occupation_id.name,
                 'introduction' : consult_window_detail.expert_user_id.introduction,
                 'categories' : category_list,
