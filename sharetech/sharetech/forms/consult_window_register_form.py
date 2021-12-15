0b1dfc7d39e0cf2150ac360fae22ca6eef29e300
masaIshik <56119425+masaIshik@users.noreply.github.com>
2021-12-15 17:23:40 +0900

    Revert "Revert "相談窓口登録機能（20211204）""

---
+++ sharetech/sharetech/forms/consult_window_register_form.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.forms import ModelForm
+from sharetech.models.consult_window import ConsultWindow
+
+class ConsultWindowRegisterForm(ModelForm):
+    '''
+    専門家登録窓口フォーム
+    '''
+
+    class Meta:
+        model = ConsultWindow
+        fields = [
+            'consult_window_title', 
+            'consult_window_overview', 
+            'consult_price', 
+            'archivement',
+            ]
+        widgets = {
+            'consult_window_overview': forms.Textarea(), # 相談窓口概要
+            'consult_price': forms.TextInput(attrs={'readonly':'readonly'}), # アルファ版のみ相談料金をreadonly
+            'archivement': forms.Textarea(), # 実績
+        }
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+    
+    def get_user(self):
+        return self.user_cache
