edddc2d8a10835340919435d38a16ca6170b0fb3
niu atsushi <atsushi.new414@gmail.com>
2021-12-22 03:25:04 +0900

    ajax処理修正

--- sharetech/sharetech/views/async_consult_window_view.py
+++ sharetech/sharetech/views/async_consult_window_view.py
@@ -15,14 +15,19 @@ class AsyncArticleLoadView(BasePageCommonView):
         LARGE = 36
 
     def get(self, request):
-        article_start_index = int(request.GET.get('index')) * self.LoadNum.SMALL + 1
-        article_end_index = article_start_index + self.LoadNum.SMALL
-        article_object_list = list(ConsultWindow.objects.order_by('-created_at')[article_start_index:article_end_index])
+        # ajaxリクエストか否か判定
+        if self.request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            article_start_index = int(request.GET.get('index')) * self.LoadNum.SMALL + 1
+            article_end_index = article_start_index + self.LoadNum.SMALL
+            article_object_list = list(ConsultWindow.objects.order_by('-created_at')[article_start_index:article_end_index])
 
-        load_article = {
-            'discover_article' : self.create_consult_window_list(article_object_list),
-        }
+            load_article = {
+                'discover_article' : self.create_consult_window_list(article_object_list),
+            }
 
-        return render(request, 'sharetech/async_load_template.html', load_article)
+            return render(request, 'sharetech/async_load_template.html', load_article)
+
+        # 直接GETリクエストが来た場合はトップページにリダイレクト
+        return edirect('top')
 
 async_consult_window_load = AsyncArticleLoadView.as_view()
